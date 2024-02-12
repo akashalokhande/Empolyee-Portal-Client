83c5c1847511862d8ea2347f169ba13ac4fa4f1f
AkashLokhande <akashlok97@gmail.com>
2024-02-12 17:39:21 +0530

    add_loader

--- src/component/Validation/CreateAccount.js
+++ src/component/Validation/CreateAccount.js
@@ -3,9 +3,11 @@ import { useContext, useState } from "react";
 import { MdClose } from "react-icons/md";
 import noteContext from "../home/Contex.js/NoteContext";
 import FormValidation from "./FormValidation";
+import Loader from "../Loader/Loader";
 
 function CreateAccount() {
   const cotext = useContext(noteContext);
+  const [loading, setLoading] = useState(false);
   const [errormsg, seterrormsg] = useState({});
   const [input, setinput] = useState({
     name: "",
@@ -28,6 +30,7 @@ function CreateAccount() {
     const { data } = await axios.post(url, input);
 
     try {
+      setLoading(true);
       if (data.success === true) {
         alert("Account Created Successfully");
         window.location.assign("/login");
@@ -36,6 +39,8 @@ function CreateAccount() {
       }
     } catch (error) {
       console.error(error);
+    } finally {
+      setLoading(false);
     }
 
     console.log(data);
@@ -43,87 +48,91 @@ function CreateAccount() {
 
   return (
     <>
-      <main className="container">
-        <div className="d-flex justify-content-center mt-5">
-          <h2 className="text-danger">{Message}</h2>
-        </div>
-        <div className="container-fluid mt-2 forms d-flex justify-content-center">
-          <form
-            onSubmit={(event) => submitform(event)}
-            style={{ color: "#ffffffeb" }}
-            className="bg-dark rounded-4 px-4 pb-4"
-          >
-            <div className="mt-4 d-flex justify-content-between h4">
-              <h1>Sign Up</h1>
-              <MdClose onClick={cotext.createmodal} />
-            </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <main className="container">
+          <div className="d-flex justify-content-center mt-5">
+            <h2 className="text-danger">{Message}</h2>
+          </div>
+          <div className="container-fluid mt-2 forms d-flex justify-content-center">
+            <form
+              onSubmit={(event) => submitform(event)}
+              style={{ color: "#ffffffeb" }}
+              className="bg-dark rounded-4 px-4 pb-4"
+            >
+              <div className="mt-4 d-flex justify-content-between h4">
+                <h1>Sign Up</h1>
+                <MdClose onClick={cotext.createmodal} />
+              </div>
 
-            <div className="mt-4">
-              <p className="me-2 h5">Full Name</p>
-              <input
-                type="text"
-                name="name"
-                className=""
-                // required
-                value={name}
-                size={30}
-                onChange={(event) => handlechange(event)}
-              />
-              {errormsg.name && (
-                <p className="text-danger m-0">{errormsg.name}</p>
-              )}
-            </div>
-            <div className="mt-2">
-              <p className="me-2  h5 ">Email</p>
-              <input
-                type="text"
-                name="Email"
-                // required
-                value={Email}
-                size={30}
-                onChange={(event) => handlechange(event)}
-              />
-              {errormsg.Email && (
-                <p className="text-danger m-0">{errormsg.Email}</p>
-              )}
-            </div>
-            <div className="mt-2">
-              <p className="me-2  h5 ">Mob.No</p>
-              <input
-                type="text"
-                name="Phone"
-                value={Phone}
-                // required
-                size={30}
-                onChange={(event) => handlechange(event)}
-              />
-              {errormsg.Phone && (
-                <p className="text-danger m-0">{errormsg.Phone}</p>
-              )}
-            </div>
+              <div className="mt-4">
+                <p className="me-2 h5">Full Name</p>
+                <input
+                  type="text"
+                  name="name"
+                  className=""
+                  // required
+                  value={name}
+                  size={30}
+                  onChange={(event) => handlechange(event)}
+                />
+                {errormsg.name && (
+                  <p className="text-danger m-0">{errormsg.name}</p>
+                )}
+              </div>
+              <div className="mt-2">
+                <p className="me-2  h5 ">Email</p>
+                <input
+                  type="text"
+                  name="Email"
+                  // required
+                  value={Email}
+                  size={30}
+                  onChange={(event) => handlechange(event)}
+                />
+                {errormsg.Email && (
+                  <p className="text-danger m-0">{errormsg.Email}</p>
+                )}
+              </div>
+              <div className="mt-2">
+                <p className="me-2  h5 ">Mob.No</p>
+                <input
+                  type="text"
+                  name="Phone"
+                  value={Phone}
+                  // required
+                  size={30}
+                  onChange={(event) => handlechange(event)}
+                />
+                {errormsg.Phone && (
+                  <p className="text-danger m-0">{errormsg.Phone}</p>
+                )}
+              </div>
 
-            <div className="mt-2">
-              <p className="me-2  h5">Password </p>
-              <input
-                type="password"
-                name="password"
-                // required
-                value={password}
-                size={30}
-                onChange={(event) => handlechange(event)}
-              />
-              {errormsg.password && (
-                <p className="text-danger m-0">{errormsg.password}</p>
-              )}
-            </div>
-            <div className="mt-3 row d-flex justify-content-center">
-              <button className="btn btn-success col-6 fs-5 fw-semibold ">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </main>
+              <div className="mt-2">
+                <p className="me-2  h5">Password </p>
+                <input
+                  type="password"
+                  name="password"
+                  // required
+                  value={password}
+                  size={30}
+                  onChange={(event) => handlechange(event)}
+                />
+                {errormsg.password && (
+                  <p className="text-danger m-0">{errormsg.password}</p>
+                )}
+              </div>
+              <div className="mt-3 row d-flex justify-content-center">
+                <button className="btn btn-success col-6 fs-5 fw-semibold ">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </main>
+      )}
     </>
   );
 }
